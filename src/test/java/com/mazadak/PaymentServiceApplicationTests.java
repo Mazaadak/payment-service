bef7e288d4e8e7b22016c3a7dae2ad30af9b8f66
Anas Elwkel <anshesh039@gmail.com>
2025-10-09 00:37:21 +0300

    chore: init project base

---
+++ src/test/java/com/mazadak/PaymentServiceApplicationTests.java
@@ -0,0 +1,9 @@
+// src/test/java/com/mazadak/PaymentServiceApplicationTests.java
+package com.mazadak;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceApplicationTests {
+}
